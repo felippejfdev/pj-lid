0410aa7d9d7f73f0746f5200eaf8a2f85c58ac98
felippejfdev <felippedev99@gmail.com>
2024-02-29 16:42:07 -0300

    f

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+let p = document.getElementById("#p")
+let input = document.querySelector("input")
+
+function search() {
+    const bases = [
+        { name: "Ivory W130" },
+        { name: "Beige C130" },
+        { name: "Beige N150" },
+        { name: "Beige W160" },
+        { name: "Beige W180" },
+        { name: "Beige N190" },
+        { name: "Beige N200" },
+        { name: "Beige N210" },
+        { name: "Bronze W110" },
+        { name: "Bronze W150" },
+    ]
+
+
+
+}
+
+
+
